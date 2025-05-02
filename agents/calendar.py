a5bfa39da5fcb4b2b6818f42fe0bd5cee6243737
DinIskakov <dinizkakov@gmail.com>
2025-05-02 18:26:38 +0500

    Add Calendar agent

---
+++ agents/calendar.py
@@ -0,0 +1,71 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from langchain.tools import tool
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+import streamlit as st
+
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+from tools.calendar_tools import init_calendar_service, get_events, set_event
+
+
+
+load_dotenv()
+
+class CognitiveCareAgent:
+    def __init__(self):
+        llm = ChatOpenAI(model="gpt-4o", temperature=0)
+
+        
+        
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system", 
+                    """
+                    You are a friendly and autonomous Google Calendar assistant who helps users manage their schedules effectively
+
+                    You must follow several rules:
+                    1. You must be polite and helpful
+                    2. You must provide accurate information
+                    3. You must not output the time code
+
+                    If you see that there is an event at the desired time, you should think if you have to delete the event or not. There shouldn't be 2 events at
+
+                    Don't ask follow up questions, take the responsibility into your own hands on what is the best option in case of colision.
+
+                    """
+                ),
+                MessagesPlaceholder(variable_name = "messages", optional = True),
+                ("user", "{input}"),
+                ("placeholder", "{agent_scratchpad}"),
+            ]
+            )
+
+        tools = []
+        tools = [
+        get_time,
+        get_events,
+        set_event,
+        delete_event
+        ]
+        agent = create_tool_calling_agent(llm, tools, prompt)
+        self.executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+    def generate_response(self, user_input):
+
+        # Use the agent executor
+        result = self.executor.invoke({"input": user_input, "messages": st.session_state.messages})
+        return result["output"]  # Adjust based on actual output format
+
+
+if __name__ == "__main__":
+    # Example usage
+    agent = CognitiveCareAgent()
+    user_input = "What should I do to improve my cognitive health?"
+    response = agent.generate_response(user_input, [])
+    print(response)
